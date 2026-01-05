ffcb8131a2988398b104f813bccf704047f61412
jingshui127 <jingshui127@163.com>
2026-01-05 11:31:07 +0800

    修复 CS0411 类型推断错误
    第 432 行的 Query.Where() 字符串语法不兼容，改为使用 LINQ Lambda 表达式：
    
    table.FindAll().Where(p => p.ExecInfos.Any(info => info.SessionUid == sessionUid)).ToList()
    
    这样可以使用强类型检查，避免了类型推断问题

--- RdpMon/MainForm.cs
+++ RdpMon/MainForm.cs
@@ -429,7 +429,7 @@ namespace Cameyo.RdpMon
             using (var db = new LiteDatabase("Filename=" + Utils.MyPath("RdpMon.db") + ";utc=true"))
             {
                 var table = db.GetCollection<Process>("Process");
-                processes = table.Find(Query.Where("ANY($.ExecInfos[*] WHERE $.SessionUid == " + sessionUid + ")"));
+                processes = table.FindAll().Where(p => p.ExecInfos.Any(info => info.SessionUid == sessionUid)).ToList();
             }
 
             sessionProcessesLv.BeginUpdate();
